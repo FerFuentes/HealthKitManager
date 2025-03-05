ea0b1b32d60b7c845e26afa59d60e4d97b0aaa49
ferfuentes <lzfer1@me.com>
2025-03-05 13:46:40 -0800

    Update HKMWalkingActivity.swift

--- Sources/HealthKitManager/Private/Activity/HKMWalkingActivity.swift
+++ Sources/HealthKitManager/Private/Activity/HKMWalkingActivity.swift
@@ -23,8 +23,6 @@ extension HealthKitManager {
         }
         
         let query = HKObserverQuery(queryDescriptors: queryDescriptors) { _, updatedSampleTypes, completionHandler, error in
-            defer { completionHandler() }
-            
             Task {
                 if let error = error {
                     completion(.failure(error))
@@ -34,6 +32,7 @@ extension HealthKitManager {
                     completion(.success(activity))
                 }
             }
+            completionHandler()
         }
 
         healthStore.execute(query)
@@ -43,31 +42,87 @@ extension HealthKitManager {
         var steps: Double?
         var activeCalories: Double?
         var distanceMeters: Double?
-        var durationMinutes: Double?
+        var durationMinutes: Double = 0.0
         var averageHeartRate: Double?
 
         for sampleType in sampleTypes {
-            guard let quantityType = sampleType as? HKQuantityType else { continue }
-            do {
-                switch quantityType {
-                case HKQuantityType(.heartRate):
-                    averageHeartRate = try await self.getAverageHeartRate(date: date)
-                    
-                case HKQuantityType(.stepCount):
-                    steps = try await self.getStepCount(date: date)
-                    durationMinutes = try await self.getTotalDurationInMinutes(date: date)
-
-                case HKQuantityType(.distanceWalkingRunning):
-                    distanceMeters = try await self.getDistanceWalkingRunning(date: date, unit: .meter())
-
-                case HKQuantityType(.activeEnergyBurned):
-                    activeCalories = try await self.getActiveEnergyBurned(date: date)
-
-                default:
-                    print("Unknown quantity type")
+            guard let quantityType = sampleType as? HKQuantityType else {
+                continue
+            }
+            switch quantityType {
+                
+            case HKQuantityType(.heartRate):
+                do {
+                    _ = try checkAuthorizationStatus(for: quantityType)
+                    averageHeartRate = try await getDescriptor(
+                        date: date,
+                        type: quantityType,
+                        options: .discreteAverage
+                    ).result(for: healthStore)
+                        .statistics(for: date)?
+                        .averageQuantity()?
+                        .doubleValue(for: HKUnit.count().unitDivided(by: HKUnit.minute()))
+                } catch {
+                    debugPrint("Error fetching heart rate: \(error.localizedDescription)")
                 }
-            } catch {
-                debugPrint("Error fetching \(quantityType.identifier): \(error.localizedDescription)")
+                
+            case HKQuantityType(.stepCount):
+                do {
+                    _ = try checkAuthorizationStatus(for: quantityType)
+                    steps = try await getDescriptor(
+                        date: date,
+                        type: quantityType,
+                        options: .cumulativeSum
+                    ).result(for: healthStore)
+                        .statistics(for: date)?
+                        .sumQuantity()?
+                        .doubleValue(for: HKUnit.count())
+                    
+                    let predicate = getPredicate(date: date)
+                    
+                    let totalDurationDescriptor = HKSampleQueryDescriptor(
+                        predicates: [.quantitySample(type: quantityType, predicate: predicate)],
+                        sortDescriptors: [SortDescriptor(\.endDate, order: .reverse)]
+                    )
+                    
+                    let stepSamples = try await totalDurationDescriptor.result(for: healthStore)
+                    durationMinutes += stepSamples
+                        .reduce(0) { $0 + $1.endDate.timeIntervalSince($1.startDate) } / 60.0
+                } catch {
+                    debugPrint("Error fetching step count: \(error.localizedDescription)")
+                }
+                
+            case HKQuantityType(.distanceWalkingRunning):
+                do {
+                    _ = try checkAuthorizationStatus(for: quantityType)
+                    distanceMeters = try await getDescriptor(
+                        date: date,
+                        type: quantityType,
+                        options: .cumulativeSum
+                    ).result(for: healthStore)
+                        .statistics(for: date)?
+                        .sumQuantity()?
+                        .doubleValue(for: HKUnit.meter())
+                } catch {
+                    debugPrint("Error fetching distance: \(error.localizedDescription)")
+                }
+                
+            case HKQuantityType(.activeEnergyBurned):
+                do {
+                    _ = try checkAuthorizationStatus(for: quantityType)
+                    activeCalories = try await getDescriptor(
+                        date: date,
+                        type: quantityType,
+                        options: .cumulativeSum
+                    ).result(for: healthStore)
+                        .statistics(for: date)?
+                        .sumQuantity()?
+                        .doubleValue(for: HKUnit.kilocalorie())
+                } catch {
+                    debugPrint("Error fetching active calories: \(error.localizedDescription)")
+                }
+            default:
+                print("Unknown quantity type")
             }
         }
 
