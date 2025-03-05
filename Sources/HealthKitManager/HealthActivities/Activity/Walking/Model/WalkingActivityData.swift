083f8631c0fad8449dc15fc56f619db68695c22f
ferfuentes <lzfer1@me.com>
2025-03-04 16:53:24 -0800

    Update WalkingActivityData.swift

--- Sources/HealthKitManager/HealthActivities/Activity/Walking/Model/WalkingActivityData.swift
+++ Sources/HealthKitManager/HealthActivities/Activity/Walking/Model/WalkingActivityData.swift
@@ -14,13 +14,4 @@ public struct WalkingActivityData: @unchecked Sendable, Codable {
     public let distanceMeters: Double?
     public let durationMinutes: Double?
     public let averageHeartRate: Double?
-    
-    public init(date: Date?, steps: Double?, activeCalories: Double?, distanceMeters: Double?, durationMinutes: Double?, averageHeartRate: Double?) {
-        self.date = date
-        self.steps = steps
-        self.activeCalories = activeCalories
-        self.distanceMeters = distanceMeters
-        self.durationMinutes = durationMinutes
-        self.averageHeartRate = averageHeartRate
-    }
 }
